09d2f8403d36befe67d24950272e5084679ec45b
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-10-19 21:07:31 +0200

    Week 8

---
+++ shema.py
@@ -0,0 +1,7 @@
+# alx_backend_graphql_crm/schema.py
+import graphene
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(default_value="Hello, GraphQL!")
+
+schema = graphene.Schema(query=Query)
