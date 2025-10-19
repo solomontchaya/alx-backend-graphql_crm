032bd00da3c9a6596b38622926738db024e860d4
Solomon Tchaya <tchayasolomon2000@gmail.com>
2025-10-19 21:26:02 +0200

    Week 8

--- crm/schema.py
+++ crm/schema.py
@@ -7,7 +7,7 @@ from graphql import GraphQLError
 class CustomerType(DjangoObjectType):
     class Meta:
         model = Customer
-        fields = "__all__"
+        fields = ("id", "name", "email", "phone")
 
 class ProductType(DjangoObjectType):
     class Meta:
@@ -18,3 +18,9 @@ class OrderType(DjangoObjectType):
     class Meta:
         model = Order
         fields = "__all__"
+
+class Query(graphene.ObjectType):
+    all_customers = graphene.List(CustomerType)
+
+    def resolve_all_customers(root, info):
+        return Customer.objects.all()
